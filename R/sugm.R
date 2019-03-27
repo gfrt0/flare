00f024dcf892fb3abd743de1904c9d5de6a24a81
cmlrqn <forteg100@gmail.com>
2019-03-27 10:46:17 +0000

    attempting to parallelise sugm.tiger.ladm.scr.R

--- R/sugm.R
+++ R/sugm.R
@@ -1,10 +1,14 @@
 #----------------------------------------------------------------------------------#
 # Package: flare                                                                   #
 # sugm(): The user interface for sugm()                                            #
+# Original
 # Author: Xingguo Li                                                               #
 # Email: <xingguo.leo@gmail.com>                                                   #
-# Date: Dec 2nd 2013                                                               #
-# Version: 1.1.0                                                                   #
+# Date: Dec 2nd 2013
+# ------
+# Modified
+# Author: Giuseppe Forte                                                           #
+# Date: Mar 27th 2019                                                              #
 #----------------------------------------------------------------------------------#
 
 sugm <- function(data,
@@ -19,120 +23,112 @@ sugm <- function(data,
                  max.ite = 1e4,
                  standardize = FALSE,
                  perturb = TRUE,
-                 verbose = TRUE)
+                 verbose = TRUE,
+                 doPar.clusters = NULL)
 {
   if(verbose) {
-    cat("High-deimensional Sparse Undirected Graphical Models.\n")
-  }
+      cat("High-dimensional Sparse Undirected Graphical Models.\n")
+    }
   if(method!="clime" && method!="tiger") {
-    cat("\"method\" must be either \"clime\" or \"tiger\" \n")
-    return(NULL)
-  }
+      cat("\"method\" must be either \"clime\" or \"tiger\" \n")
+      return(NULL)
+    }
   n = nrow(data)
   d = ncol(data)
   if(method == "tiger" && d<3){
-    cat("d>=3 is required for \"tiger\" \n")
-    cat("More on help(sugm) \n")
-    return(NULL)
-  }
+      cat("d>=3 is required for \"tiger\" \n")
+      cat("More on help(sugm) \n")
+      return(NULL)
+    }
   if(method == "clime" && d<2){
-    cat("d>=2 is required for \"clime\" \n")
-    cat("More on help(sugm) \n")
-    return(NULL)
-  }
+      cat("d>=2 is required for \"clime\" \n")
+      cat("More on help(sugm) \n")
+      return(NULL)
+    }
   maxdf = max(n,d)
   est = list()
   est$cov.input = isSymmetric(data)
-  if(est$cov.input)
-  {
-    if(verbose) {
-      cat("The input is identified as the covriance matrix.\n")
-    }
-    if(sum(is.na(data))>0){
-      cat("The input has missing values for covariance/correlation input.\n")
-      return(NULL)
-    }
-    if(method=="tiger") {
-      cat("The input for \"tiger\" cannot be covriance matrix.\n")
-      return(NULL)
-    }
-    if(standardize){
-      S0 = data
-      diag.cov=diag(S0)
-      diag.cov.invsq = diag(1/sqrt(diag.cov))
-      S = diag.cov.invsq%*%S0%*%diag.cov.invsq
-    }
-    else{
-      S0 = data
-      S = S0
-    }
-  }
-  if(!est$cov.input)
-  {
-    X0=data
-    if(method=="tiger" && sum(is.na(X0))>0){
-      cat("The input for \"tiger\" has missing values.\n")
-      return(NULL)
-    }
-    X1 = X0 - matrix(rep(colMeans(X0),n), nrow=n, byrow=TRUE)
-    S0 = crossprod(X1)/(n-1)
-    diag.cov=diag(S0)
-    diag.cov.invsq = diag(1/sqrt(diag.cov))
-    if(method=="tiger") {
-      data = X1%*%diag.cov.invsq
-      S = S0
-    }else{
+  if(est$cov.input) {
+      if(verbose) {
+        cat("The input is identified as the covriance matrix.\n")
+      }
+      if(sum(is.na(data))>0){
+        cat("The input has missing values for covariance/correlation input.\n")
+        return(NULL)
+      }
+      if(method=="tiger") {
+        cat("The input for \"tiger\" cannot be covriance matrix.\n")
+        return(NULL)
+      }
       if(standardize){
-#         S = diag.cov.invsq%*%S0%*%diag.cov.invsq
-        S = cor(X0,use="pairwise.complete.obs")
-        diag.cov=diag(S)
+        S0 = data
+        diag.cov=diag(S0)
         diag.cov.invsq = diag(1/sqrt(diag.cov))
-      }else{
-#         S = S0
-        S = cov(X0,use="pairwise.complete.obs")
+        S = diag.cov.invsq%*%S0%*%diag.cov.invsq
+      } else {
+        S0 = data
+        S = S0
       }
+    } else {
+        X0=data
+        if(method=="tiger" && sum(is.na(X0))>0){
+            cat("The input for \"tiger\" has missing values.\n")
+            return(NULL)
+          }
+        X1 = X0 - matrix(rep(colMeans(X0),n), nrow=n, byrow=TRUE)
+        S0 = crossprod(X1)/(n-1)
+        diag.cov=diag(S0)
+        diag.cov.invsq = diag(1/sqrt(diag.cov))
+        if(method=="tiger") {
+            data = X1%*%diag.cov.invsq
+            S = S0
+          } else {
+          if(standardize){
+    #         S = diag.cov.invsq%*%S0%*%diag.cov.invsq
+              S = cor(X0,use="pairwise.complete.obs")
+              diag.cov=diag(S)
+              diag.cov.invsq = diag(1/sqrt(diag.cov))
+            } else {
+    #         S = S0
+            S = cov(X0,use="pairwise.complete.obs")
+            }
+        }
     }
-  }
-  
+
   if(!is.null(lambda)) nlambda = length(lambda)
-  if(is.null(lambda))
-  {
-    if(method == "tiger") {
-      if(is.null(nlambda)){
-        nlambda = 5
-      }
-      if(is.null(lambda.min.ratio))
-        lambda.min.ratio = 0.4
-      lambda.max= pi*sqrt(log(d)/n)
-      lambda = seq(lambda.max,lambda.min.ratio*lambda.max,length=nlambda)
+  if(is.null(lambda)) {
+      if(method == "tiger") {
+          if(is.null(nlambda)){
+            nlambda = 5
+          }
+          if(is.null(lambda.min.ratio)) lambda.min.ratio = 0.4
+          lambda.max= pi*sqrt(log(d)/n)
+          lambda = seq(lambda.max,lambda.min.ratio*lambda.max,length=nlambda)
+      } else {
+          if(is.null(nlambda)) nlambda = 5
+          if(is.null(lambda.min.ratio)) lambda.min.ratio = 0.4
+          lambda.max.tmp1 = min(max(S-diag(diag(S))),-min(S-diag(diag(S))))
+          lambda.max.tmp2 = max(max(S-diag(diag(S))),-min(S-diag(diag(S))))
+          if(lambda.max.tmp1==0)
+            lambda.max = lambda.max.tmp2
+          else
+            lambda.max = lambda.max.tmp1
+          lambda.min = lambda.min.ratio*lambda.max
+          lambda = exp(seq(log(lambda.max), log(lambda.min), length = nlambda))
+          rm(lambda.max,lambda.min,lambda.min.ratio)
+          gc()
+        }
     }
-    else {
-      if(is.null(nlambda))
-        nlambda = 5
-      if(is.null(lambda.min.ratio))
-        lambda.min.ratio = 0.4
-      lambda.max.tmp1 = min(max(S-diag(diag(S))),-min(S-diag(diag(S))))
-      lambda.max.tmp2 = max(max(S-diag(diag(S))),-min(S-diag(diag(S))))
-      if(lambda.max.tmp1==0)
-        lambda.max = lambda.max.tmp2
-      else
-        lambda.max = lambda.max.tmp1
-      lambda.min = lambda.min.ratio*lambda.max
-      lambda = exp(seq(log(lambda.max), log(lambda.min), length = nlambda))
-      rm(lambda.max,lambda.min,lambda.min.ratio)
-      gc()
-    }
-  }
-  
+
   est$lambda = lambda
   est$nlambda = nlambda
-  
+
   if(is.null(rho))
     rho = 1
   begt = Sys.time()
   if(method == "clime"){
     if (is.logical(perturb)) {
-      if (perturb) { 
+      if (perturb) {
         #eigvals = eigen(S, only.values=T)$values
         #perturb = max(max(max(eigvals) - d*min(eigvals), 0)/(d-1), 1/n)
         perturb = 1/sqrt(n)
@@ -145,7 +141,7 @@ sugm <- function(data,
       if(is.null(shrink)) shrink = 0#1.5
       if(is.null(max.ite)) max.ite=1e4
       re.sugm = sugm.clime.ladm.scr(S, lambda, nlambda, n, d, maxdf, rho, shrink, prec, max.ite, verbose)
-      
+
       if(standardize){
         for(i in 1:nlambda){
           re.sugm$icov1[[i]] = diag.cov.invsq%*%re.sugm$icov1[[i]]%*%diag.cov.invsq
@@ -160,13 +156,15 @@ sugm <- function(data,
       }
     }
   }
-  
+
   if(method == "tiger"){
     if(is.null(shrink)) shrink=0
     if(is.null(max.ite)) max.ite=1e4
-    
-    re.sugm = sugm.tiger.ladm.scr(data, n, d, maxdf, rho, lambda, shrink, prec, max.ite, verbose)
-    
+
+    # The actual computation step
+    re.sugm = sugm.tiger.ladm.scr(data, n, d, maxdf, rho, lambda, shrink, prec, max.ite,
+                                  verbose, doPar.clusters)
+
     for(i in 1:nlambda){
       re.sugm$icov1[[i]] = diag.cov.invsq%*%re.sugm$icov1[[i]]%*%diag.cov.invsq
       icov.i = re.sugm$icov1[[i]]
@@ -175,8 +173,8 @@ sugm <- function(data,
   }
   est$ite = re.sugm$ite
   runt = Sys.time()-begt
-  
-  for(j in 1:d) {
+
+  for(j in 1:d) { # d = ncol(data)
     if(re.sugm$col.cnz[j+1]>re.sugm$col.cnz[j])
     {
       idx.tmp = (re.sugm$col.cnz[j]+1):re.sugm$col.cnz[j+1]
@@ -187,16 +185,16 @@ sugm <- function(data,
   }
   G = new("dgCMatrix", Dim = as.integer(c(d*nlambda,d)), x = as.vector(re.sugm$x[1:re.sugm$col.cnz[d+1]]),
           p = as.integer(re.sugm$col.cnz), i = as.integer(re.sugm$row.idx[1:re.sugm$col.cnz[d+1]]))
-  
+
   est$beta = list()
   est$path = list()
-  est$df = matrix(0,d,nlambda)  
-  est$sparsity = rep(0,nlambda)  
+  est$df = matrix(0,d,nlambda)
+  est$sparsity = rep(0,nlambda)
   for(i in 1:nlambda) {
     est$beta[[i]] = G[((i-1)*d+1):(i*d),]
     est$path[[i]] = abs(est$beta[[i]])
     est$df[,i] = apply(sign(est$path[[i]]),2,sum)
-    
+
     if(sym == "or")
       est$path[[i]] = sign(est$path[[i]] + t(est$path[[i]]))
     if(sym == "and")
@@ -221,7 +219,7 @@ sugm <- function(data,
 }
 
 print.sugm <- function(x, ...)
-{  
+{
   cat("\n sugm options summary: \n")
   cat(x$nlambda, " lambdas used:\n")
   print(signif(x$lambda,digits=3))
@@ -233,44 +231,44 @@ print.sugm <- function(x, ...)
 
 plot.sugm = function(x, align = FALSE, ...){
   gcinfo(FALSE)
-  
+
   if(x$nlambda == 1)	par(mfrow = c(1, 2), pty = "s", omi=c(0.3,0.3,0.3,0.3), mai = c(0.3,0.3,0.3,0.3))
   if(x$nlambda == 2)	par(mfrow = c(1, 3), pty = "s", omi=c(0.3,0.3,0.3,0.3), mai = c(0.3,0.3,0.3,0.3))
   if(x$nlambda >= 3)	par(mfrow = c(1, 4), pty = "s", omi=c(0.3,0.3,0.3,0.3), mai = c(0.3,0.3,0.3,0.3))
-  
+
   if(x$nlambda <= 3)	z.final = 1:x$nlambda
-  
+
   if(x$nlambda >=4){
     z.max = max(x$sparsity)
     z.min = min(x$sparsity)
     z = z.max - z.min
     z.unique = unique(c(which(x$sparsity>=(z.min + 0.03*z))[1],which(x$sparsity>=(z.min + 0.07*z))[1],which(x$sparsity>=(z.min + 0.15*z))[1]))
-    
-    
+
+
     if(length(z.unique) == 1){
       if(z.unique<(x$nlambda-1))	z.final = c(z.unique,z.unique+1,z.unique+2)
       if(z.unique==(x$nlambda-1)) z.final = c(z.unique-1,z.unique,z.unique+1)
       if(z.unique==x$nlambda) 	z.final = c(z.unique-2,z.unique-1,z.unique)
     }
-    
+
     if(length(z.unique) == 2){
       if(diff(z.unique)==1){
-        if(z.unique[2]<x$nlambda) z.final = c(z.unique,z.unique[2]+1) 
+        if(z.unique[2]<x$nlambda) z.final = c(z.unique,z.unique[2]+1)
         if(z.unique[2]==x$nlambda) z.final = c(z.unique[1]-1,z.unique)
       }
       if(diff(z.unique)>1) z.final = c(z.unique[1],z.unique[1]+1,z.unique[2])
     }
-    
+
     if(length(z.unique) == 3) z.final = z.unique
-    
+
     rm(z.max,z.min,z,z.unique)
     gc()
-    
+
   }
   plot(x$lambda, x$sparsity, log = "x", xlab = "Regularization Parameter", ylab = "Sparsity Level", type = "l",xlim = rev(range(x$lambda)), main = "Sparsity vs. Regularization")
-  
+
   lines(x$lambda[z.final],x$sparsity[z.final],type = "p")
-  
+
   if(align){
     layout.grid = layout.fruchterman.reingold(graph.adjacency(as.matrix(x$path[[z.final[length(z.final)]]]), mode="undirected", diag=FALSE))
     for(i in z.final){
--- R/sugm.tiger.ladm.scr.R
+++ R/sugm.tiger.ladm.scr.R
@@ -6,10 +6,15 @@
 # Emails: <xingguo.leo@gmail.com>                                                  #
 # Date: Jan 3rd, 2018                                                              #
 # Version: 1.6.0                                                                   #
+# ------
+# Modified
+# Author: Giuseppe Forte                                                           #
+# Date: Mar 27th 2019                                                              #
 #----------------------------------------------------------------------------------#
 
-sugm.tiger.ladm.scr <- function(data, n, d, maxdf, rho, lambda, shrink, prec, max.ite, verbose){
-  
+sugm.tiger.ladm.scr <- function(data, n, d, maxdf, rho, lambda,
+                                shrink, prec, max.ite, verbose, doPar.clusters){
+
   if(verbose==TRUE)
     cat("Tuning-Insensitive Graph Estimation and Regression.\n")
   Z = data
@@ -49,53 +54,55 @@ sugm.tiger.ladm.scr <- function(data, n, d, maxdf, rho, lambda, shrink, prec, ma
   for(i in 1:nlambda){
     icov.list1[[i]] = matrix(0,d,d)
   }
-  for(j in 1:d){
-    Z.j = Z[,j]
-    Z.resj = Z[,-j]
-    Zy = ZZ[-j,j]
-    idx.scr0 = order(Zy)
-    idx.scr1 = idx.scr0[1:num.scr1]
-    idx.scr2 = idx.scr0[1:num.scr2]
-    ZZ0 = ZZ[-j,-j]
-    Z.order = ZZ0[idx.scr0,idx.scr0]
-    gamma = max(colSums(abs(Z.order)))
-    icov0 = rep(0,d*nlambda)
-    ite0.int = rep(0,nlambda)
-    ite0.int1 = rep(0,nlambda)
-    ite0.int2 = rep(0,nlambda)
-    ite0.int3 = rep(0,nlambda)
-    ite0.int4 = rep(0,nlambda)
-    ite0.int5 = rep(0,nlambda)
-    x0 = rep(0,maxdf*nlambda)
-    col.cnz0 = 0
-    row.idx0 = rep(0,maxdf*nlambda)
-    str=.C("sugm_tiger_ladm_scr", as.double(Z.j), as.double(Z.resj), 
-           as.double(Zy), 
-           as.double(Z.order), as.double(icov0), as.double(x0), as.integer(d),
-           as.integer(n), as.double(gamma), as.double(lambda), as.integer(nlambda), 
-           as.double(rho), as.integer(col.cnz0), as.integer(row.idx0), 
-           as.integer(ite0.int), as.integer(ite0.int1), as.integer(ite0.int2), 
-           as.integer(ite0.int3), as.integer(ite0.int4), as.integer(ite0.int5), 
-           as.integer(num.scr1), as.integer(num.scr2), 
-           as.integer(idx.scr0), as.integer(idx.scr1), as.integer(idx.scr2), 
-           as.integer(max.ite), as.double(prec), as.integer(j), PACKAGE="flare")
-    icov = matrix(unlist(str[5]), byrow = FALSE, ncol = nlambda)
-    for(i in 1:nlambda){
-      icov.list1[[i]][,j] = icov[,i]
+  is (!is.null(doPar.clusters) doParallel::registerDoParallel(doPar.clusters)
+  foreach (j = 1:d) { # d = ncol(Z)
+      Z.j = Z[,j]
+      Z.resj = Z[,-j]
+      Zy = ZZ[-j,j]
+      idx.scr0 = order(Zy)
+      idx.scr1 = idx.scr0[1:num.scr1]
+      idx.scr2 = idx.scr0[1:num.scr2]
+      ZZ0 = ZZ[-j,-j]
+      Z.order = ZZ0[idx.scr0,idx.scr0]
+      gamma = max(colSums(abs(Z.order)))
+      icov0 = rep(0,d*nlambda)
+      ite0.int = rep(0,nlambda)
+      ite0.int1 = rep(0,nlambda)
+      ite0.int2 = rep(0,nlambda)
+      ite0.int3 = rep(0,nlambda)
+      ite0.int4 = rep(0,nlambda)
+      ite0.int5 = rep(0,nlambda)
+      x0 = rep(0,maxdf*nlambda)
+      col.cnz0 = 0
+      row.idx0 = rep(0,maxdf*nlambda)
+      str=.C("sugm_tiger_ladm_scr", as.double(Z.j), as.double(Z.resj),
+             as.double(Zy),
+             as.double(Z.order), as.double(icov0), as.double(x0), as.integer(d),
+             as.integer(n), as.double(gamma), as.double(lambda), as.integer(nlambda),
+             as.double(rho), as.integer(col.cnz0), as.integer(row.idx0),
+             as.integer(ite0.int), as.integer(ite0.int1), as.integer(ite0.int2),
+             as.integer(ite0.int3), as.integer(ite0.int4), as.integer(ite0.int5),
+             as.integer(num.scr1), as.integer(num.scr2),
+             as.integer(idx.scr0), as.integer(idx.scr1), as.integer(idx.scr2),
+             as.integer(max.ite), as.double(prec), as.integer(j), PACKAGE="flare")
+      icov = matrix(unlist(str[5]), byrow = FALSE, ncol = nlambda)
+      for(i in 1:nlambda){
+        icov.list1[[i]][,j] = icov[,i]
+      }
+      cnt = unlist(str[13])
+      col.cnz[j+1] = cnt+col.cnz[j]
+      if(cnt>0){
+        x[(col.cnz[j]+1):col.cnz[j+1]] = unlist(str[6])[1:cnt]
+        row.idx[(col.cnz[j]+1):col.cnz[j+1]] = unlist(str[14])[1:cnt]
+      }
+      ite.int[j,] = unlist(str[15])
+      ite.int1[j,] = unlist(str[16])
+      ite.int2[j,] = unlist(str[17])
+      ite.int3[j,] = unlist(str[18])
+      ite.int4[j,] = unlist(str[19])
+      ite.int5[j,] = unlist(str[20])
     }
-    cnt = unlist(str[13])
-    col.cnz[j+1] = cnt+col.cnz[j]
-    if(cnt>0){
-      x[(col.cnz[j]+1):col.cnz[j+1]] = unlist(str[6])[1:cnt]
-      row.idx[(col.cnz[j]+1):col.cnz[j+1]] = unlist(str[14])[1:cnt]
-    }
-    ite.int[j,] = unlist(str[15])
-    ite.int1[j,] = unlist(str[16])
-    ite.int2[j,] = unlist(str[17])
-    ite.int3[j,] = unlist(str[18])
-    ite.int4[j,] = unlist(str[19])
-    ite.int5[j,] = unlist(str[20])
-  }
+  is (!is.null(doPar.clusters) doParallel::registerDoSEQ
   icov.list = vector("list", nlambda)
   for(i in 1:nlambda){
     icov.i = icov.list1[[i]]
